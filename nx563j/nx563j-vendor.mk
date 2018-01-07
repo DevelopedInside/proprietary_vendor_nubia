0743d7a595e38b2c117595c56cb7a6853468c463
siyuansixsixsix <siyuansixsixsix@gmail.com>
2018-01-07 17:20:33 +0800

    fix nfc and vibrate

--- nx563j/nx563j-vendor.mk
+++ nx563j/nx563j-vendor.mk
@@ -80,6 +80,7 @@ PRODUCT_COPY_FILES += \
     vendor/nubia/nx563j/proprietary/lib/hw/camera.msm8998.so:system/lib/hw/camera.msm8998.so \
     vendor/nubia/nx563j/proprietary/lib/hw/consumerir.default.so:system/lib/hw/consumerir.default.so \
     vendor/nubia/nx563j/proprietary/lib/hw/fingerprint.msm8998.so:system/lib/hw/fingerprint.msm8998.so \
+	vendor/nubia/nx563j/proprietary/lib/hw/vibrator.default.so:system/lib/hw/vibrator.default.so \
     vendor/nubia/nx563j/proprietary/lib/libAlgorithm.so:system/lib/libAlgorithm.so \
     vendor/nubia/nx563j/proprietary/lib/libArcsoftAlgorithm.so:system/lib/libArcsoftAlgorithm.so \
     vendor/nubia/nx563j/proprietary/lib/libBigAperture.so:system/lib/libBigAperture.so \
@@ -127,6 +128,8 @@ PRODUCT_COPY_FILES += \
     vendor/nubia/nx563j/proprietary/lib/libqomx_core.so:system/lib/libqomx_core.so \
     vendor/nubia/nx563j/proprietary/lib64/hw/consumerir.default.so:system/lib64/hw/consumerir.default.so \
     vendor/nubia/nx563j/proprietary/lib64/hw/fingerprint.msm8998.so:system/lib64/hw/fingerprint.msm8998.so \
+	vendor/nubia/nx563j/proprietary/lib64/hw/vibrator.default.so:system/lib64/hw/vibrator.default.so \
+	vendor/nubia/nx563j/proprietary/lib64/hw/nfc_nci.bcm2079x.default.so:system/lib64/hw/nfc_nci.bcm2079x.default.so \
     vendor/nubia/nx563j/proprietary/lib64/libAlgorithm.so:system/lib64/libAlgorithm.so \
     vendor/nubia/nx563j/proprietary/lib64/libArcsoftAlgorithm.so:system/lib64/libArcsoftAlgorithm.so \
     vendor/nubia/nx563j/proprietary/lib64/libBigAperture.so:system/lib64/libBigAperture.so \
