88bd7a3b597939aa35635bbcb2665266b454b8eb
BeYkeRYkt <beykerykt@gmail.com>
2018-03-22 00:35:57 +0900

    nx563j: move prebuilt kernel blobs to specific folder
    Change-Id: If5133880b93b33a8f0ab70168a8c1724b11f8564

--- nx563j/nx563j-vendor.mk
+++ nx563j/nx563j-vendor.mk
@@ -117,17 +117,6 @@ PRODUCT_COPY_FILES += \
     vendor/nubia/nx563j/proprietary/lib/libproductinfo.so:system/lib/libproductinfo.so \
     vendor/nubia/nx563j/proprietary/lib/libqomx_core.so:system/lib/libqomx_core.so \
     vendor/nubia/nx563j/proprietary/lib/libteeclientjni.so:system/lib/libteeclientjni.so \
-    vendor/nubia/nx563j/proprietary/lib/modules/br_netfilter.ko:system/lib/modules/br_netfilter.ko \
-    vendor/nubia/nx563j/proprietary/lib/modules/mpq-adapter.ko:system/lib/modules/mpq-adapter.ko \
-    vendor/nubia/nx563j/proprietary/lib/modules/mpq-dmx-hw-plugin.ko:system/lib/modules/mpq-dmx-hw-plugin.ko \
-    vendor/nubia/nx563j/proprietary/lib/modules/multi_channel_hook.ko:system/lib/modules/multi_channel_hook.ko \
-    vendor/nubia/nx563j/proprietary/lib/modules/wlan.ko:system/lib/modules/wlan.ko \
-    vendor/nubia/nx563j/proprietary/lib/modules/rdbg.ko:system/lib/modules/rdbg.ko \
-    vendor/nubia/nx563j/proprietary/lib/modules/test-iosched.ko:system/lib/modules/test-iosched.ko \
-    vendor/nubia/nx563j/proprietary/lib/modules/texfat.ko:system/lib/modules/texfat.ko \
-    vendor/nubia/nx563j/proprietary/lib/modules/tspp.ko:system/lib/modules/tspp.ko \
-    vendor/nubia/nx563j/proprietary/lib/modules/ufs_test.ko:system/lib/modules/ufs_test.ko \
-    vendor/nubia/nx563j/proprietary/lib/modules/wil6210.ko:system/lib/modules/wil6210.ko \
     vendor/nubia/nx563j/proprietary/lib64/hw/consumerir.default.so:system/lib64/hw/consumerir.default.so \
     vendor/nubia/nx563j/proprietary/lib64/hw/fingerprint.msm8998.so:system/lib64/hw/fingerprint.msm8998.so \
     vendor/nubia/nx563j/proprietary/lib64/hw/nfc_nci.bcm2079x.default.so:system/lib64/hw/nfc_nci.bcm2079x.default.so \
